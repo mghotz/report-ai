c76906f4b55df849fe0b5269ad5817a44d5cf2b2
Mahammad <salimovm.7@gmail.com>
2024-04-06 22:50:24 +0400

    run_query function for running queries in sqlite demo db

---
+++ tools/sql.py
@@ -0,0 +1,19 @@
+from langchain.tools import Tool
+import sqlite3
+
+def sqlite_conn():
+    conn = sqlite3.connect("db.sqlite")
+    return conn
+
+def run_query(query):
+    cur = conn.cursor()
+    cur.execute(query)
+    result = cur.fetchall()
+    return result
+
+
+run_query_tool = Tool.from_function(
+    name="run_query",
+    description="run a sqlite query",
+    func=run_query
+)
