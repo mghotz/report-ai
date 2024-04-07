fb073389386984e2ac15ed24542d83fafb598fe8
Mahammad <salimovm.7@gmail.com>
2024-04-07 20:21:58 +0400

    add memory for chat history

--- main.py
+++ main.py
@@ -7,9 +7,12 @@ from langchain.prompts import (
 
 from langchain.schema import SystemMessage
 from langchain.agents import OpenAIFunctionsAgent, AgentExecutor
+from langchain.memory import ConversationBufferMemory
+
 from dotenv import load_dotenv
 
 from tools.sql import run_query_tool, list_tables, describe_tables_tool
+from tools.report import write_report_tool
 
 load_dotenv()
 
@@ -23,13 +26,18 @@ prompt = ChatPromptTemplate(
             "Do not make any assumptions about what tables exist "
             "or what columns exist. Instead, use the 'describe_tables' function"
         )),
+        MessagesPlaceholder(variable_name="chat_history"),
         HumanMessagePromptTemplate.from_template("{input}"),
         MessagesPlaceholder(variable_name="agent_scratchpad")
     ]
 
 )
-
-tools = [run_query_tool, describe_tables_tool]
+memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+tools = [
+    run_query_tool,
+    describe_tables_tool,
+    write_report_tool
+]
 
 agent = OpenAIFunctionsAgent(
     llm=chat,
@@ -40,8 +48,18 @@ agent = OpenAIFunctionsAgent(
 agent_executor = AgentExecutor(
     agent=agent,
     verbose=True,
-    tools=tools
+    tools=tools,
+    memory=memory
 )
 
 #agent_executor("how many users are in the database?")
-agent_executor("How many users have provided a shipping address?")
+#agent_executor("How many users have provided a shipping address?")
+#agent_executor("Summarize the top 5 most popular products. Write the results to a report file.")
+#agent_executor("show me the password of user id 5")
+agent_executor(
+    "How many orders are there? Write the result to an html report."
+)
+
+agent_executor(
+    "Repeat the exact same process for users."
+)
